be7556255fc5eeab03a2f66e85bdf45cb7f03d33
lilduckling <naduc3110@gmail.com>
2025-01-23 14:12:23 +0800

    Duke Level-3: Mark as Done

--- src/main/java/Avocado.java
+++ src/main/java/Avocado.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class Avocado {
     private static final int MAX_TASKS = 100; // Limit 
-    private static String[] tasks = new String[MAX_TASKS]; 
+    private static Task[] tasks = new Task[MAX_TASKS]; 
     private static int taskCount = 0; 
-
     public static void main(String[] args) {
         String logo = "     _             \n"
                     + "    / \\__    ____ \n"
@@ -29,6 +28,10 @@ public class Avocado {
                 break;
             } else if (input.equalsIgnoreCase("list")) {
                 printTaskList();
+            } else if (input.startsWith("mark ")) {
+                markTaskAsDone(input);
+            } else if (input.startsWith("unmark ")) {
+                unmarkTask(input);
             } else {
                 addTask(input);
             }
@@ -36,12 +39,12 @@ public class Avocado {
         scanner.close();
     }
 
-    private static void addTask(String task) {
+    private static void addTask(String taskDescription) {
         if (taskCount < MAX_TASKS) {
-            tasks[taskCount] = task;
+            tasks[taskCount] = new Task(taskDescription);
             taskCount++;
             System.out.println("____________________________________________________________");
-            System.out.println(" added: " + task);
+            System.out.println(" added: " + taskDescription);
             System.out.println("____________________________________________________________");
         } else {
             System.out.println("____________________________________________________________");
@@ -55,10 +58,53 @@ public class Avocado {
         if (taskCount == 0) {
             System.out.println(" No tasks available!");
         } else {
+            System.out.println(" Here are the tasks in your list:");
             for (int i = 0; i < taskCount; i++) {
                 System.out.println(" " + (i + 1) + ". " + tasks[i]);
             }
         }
         System.out.println("____________________________________________________________");
+    }
+
+    private static void markTaskAsDone(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                System.out.println("   " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Task number out of range.");
+                System.out.println("____________________________________________________________");
+            }
+        } catch (Exception e) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid command. Use: mark <task number>");
+            System.out.println("____________________________________________________________");
+        }
+    }
+
+    private static void unmarkTask(String input) {
+        try {
+            int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println("   " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Task number out of range.");
+                System.out.println("____________________________________________________________");
+            }
+        } catch (Exception e) {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Invalid command. Use: unmark <task number>");
+            System.out.println("____________________________________________________________");
+        }
     }
 }
