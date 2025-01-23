b03dad446918c83f36c2e88646b2ff2e4c71144b
lilduckling <naduc3110@gmail.com>
2025-01-23 14:06:06 +0800

    Duke Level-2: Add, List

--- src/main/java/Avocado.java
+++ src/main/java/Avocado.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Avocado {
+    private static final int MAX_TASKS = 100; // Limit 
+    private static String[] tasks = new String[MAX_TASKS]; 
+    private static int taskCount = 0; 
+
     public static void main(String[] args) {
         String logo = "     _             \n"
                     + "    / \\__    ____ \n"
@@ -15,21 +19,46 @@ public class Avocado {
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
-        String input;
-
         while (true) {
-            input = scanner.nextLine();
-            if (input.equalsIgnoreCase("bye")) { 
+            String input = scanner.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
-                break; // Exit loop
+                break;
+            } else if (input.equalsIgnoreCase("list")) {
+                printTaskList();
+            } else {
+                addTask(input);
             }
+        }
+        scanner.close();
+    }
+
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount] = task;
+            taskCount++;
             System.out.println("____________________________________________________________");
-            System.out.println("Ok i will now " + input + " like an avocado!"); 
+            System.out.println(" added: " + task);
+            System.out.println("____________________________________________________________");
+        } else {
+            System.out.println("____________________________________________________________");
+            System.out.println(" Sorry, you have reached the task limit!");
             System.out.println("____________________________________________________________");
         }
+    }
 
-        scanner.close(); 
+    private static void printTaskList() {
+        System.out.println("____________________________________________________________");
+        if (taskCount == 0) {
+            System.out.println(" No tasks available!");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            }
+        }
+        System.out.println("____________________________________________________________");
     }
 }
