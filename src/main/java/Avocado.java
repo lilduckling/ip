9e1aab4eeca3d0c992be45324cbe22628fa2286d
lilduckling <naduc3110@gmail.com>
2025-01-23 13:55:08 +0800

    Duke Level-0: Rename, Greet, Exit

---
+++ src/main/java/Avocado.java
@@ -0,0 +1,17 @@
+public class Avocado {
+    public static void main(String[] args) {
+        String logo = "     _             \n"
+                    + "    / \\__    ____ \n"
+                    + "   / _ \\ \\  / /  \\ \n"
+                    + "  / ___ \\ \\/ / |  |\n"
+                    + " /_/   \\_\\__/\\___/ \n";
+            
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Avocado");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
