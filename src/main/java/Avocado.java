79b3570ff2a5c7635e958bb3e99b9a114b7bb827
lilduckling <naduc3110@gmail.com>
2025-01-24 11:09:47 +0800

    Duke Level-6: Delete

--- src/main/java/Avocado.java
+++ src/main/java/Avocado.java
@@ -1,16 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Avocado {
-    private static final int MAX_TASKS = 100; // Limit of tasks
-    private static Task[] tasks = new Task[MAX_TASKS]; // Storage for tasks
-    private static int taskCount = 0; // Track the number of tasks
+    private static final ArrayList<Task> tasks = new ArrayList<>(); 
 
     public static void main(String[] args) {
         String logo = "     _             \n"
-                    + "    / \\__    ____ \n"
-                    + "   / _ \\ \\  / /  \\ \n"
-                    + "  / ___ \\ \\/ / |  |\n"
-                    + " /_/   \\_\\__/\\___/ \n";
+                + "    / \\__    ____ \n"
+                + "   / _ \\ \\  / /  \\ \n"
+                + "  / ___ \\ \\/ / |  |\n"
+                + " /_/   \\_\\__/\\___/ \n";
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello from\n" + logo);
@@ -34,6 +33,8 @@ public class Avocado {
                     markTaskAsDone(input);
                 } else if (input.startsWith("unmark ")) {
                     unmarkTask(input);
+                } else if (input.startsWith("delete ")) {
+                    deleteTask(input);
                 } else if (input.startsWith("todo ")) {
                     addTodo(input.substring(5).trim());
                 } else if (input.startsWith("deadline ")) {
@@ -56,63 +57,65 @@ public class Avocado {
         if (description.isEmpty()) {
             throw new AvocadoException("Oops! The description of a todo cannot be empty.");
         }
-        if (taskCount < MAX_TASKS) {
-            tasks[taskCount] = new Todo(description);
-            taskCount++;
-            System.out.println("____________________________________________________________");
-            System.out.println(" Got it. I've added this task:");
-            System.out.println("   " + tasks[taskCount - 1]);
-            System.out.println(" Now you have " + taskCount + " tasks in the list.");
-            System.out.println("____________________________________________________________");
-        } else {
-            throw new AvocadoException("Oops! Task list is full. Cannot add more tasks.");
-        }
+        tasks.add(new Todo(description));
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + tasks.get(tasks.size() - 1));
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 
     private static void addDeadline(String input) throws AvocadoException {
         String[] parts = input.split(" /by ", 2);
-        if (parts.length < 2 || parts[0].trim().isEmpty() || parts[1].trim().isEmpty()) {
+        if (parts.length < 2) {
             throw new AvocadoException("Oops! Deadline format should be: deadline <task> /by <date>");
         }
-        if (taskCount < MAX_TASKS) {
-            tasks[taskCount] = new Deadline(parts[0], parts[1]);
-            taskCount++;
-            System.out.println("____________________________________________________________");
-            System.out.println(" Got it. I've added this task:");
-            System.out.println("   " + tasks[taskCount - 1]);
-            System.out.println(" Now you have " + taskCount + " tasks in the list.");
-            System.out.println("____________________________________________________________");
-        } else {
-            throw new AvocadoException("Oops! Task list is full. Cannot add more tasks.");
-        }
+        tasks.add(new Deadline(parts[0], parts[1]));
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + tasks.get(tasks.size() - 1));
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 
     private static void addEvent(String input) throws AvocadoException {
         String[] parts = input.split(" /from | /to ", 3);
-        if (parts.length < 3 || parts[0].trim().isEmpty() || parts[1].trim().isEmpty() || parts[2].trim().isEmpty()) {
+        if (parts.length < 3) {
             throw new AvocadoException("Oops! Event format should be: event <task> /from <start> /to <end>");
         }
-        if (taskCount < MAX_TASKS) {
-            tasks[taskCount] = new Event(parts[0], parts[1], parts[2]);
-            taskCount++;
+        tasks.add(new Event(parts[0], parts[1], parts[2]));
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + tasks.get(tasks.size() - 1));
+        System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void deleteTask(String input) throws AvocadoException {
+        try {
+            int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+            if (taskIndex < 0 || taskIndex >= tasks.size()) {
+                throw new AvocadoException("Oops! Task number out of range.");
+            }
+            Task removedTask = tasks.remove(taskIndex);
             System.out.println("____________________________________________________________");
-            System.out.println(" Got it. I've added this task:");
-            System.out.println("   " + tasks[taskCount - 1]);
-            System.out.println(" Now you have " + taskCount + " tasks in the list.");
+            System.out.println(" Noted. I've removed this task:");
+            System.out.println("   " + removedTask);
+            System.out.println(" Now you have " + tasks.size() + " tasks in the list.");
             System.out.println("____________________________________________________________");
-        } else {
-            throw new AvocadoException("Oops! Task list is full. Cannot add more tasks.");
+        } catch (Exception e) {
+            throw new AvocadoException("Oops! Please provide a valid task number to delete.");
         }
     }
 
     private static void printTaskList() {
         System.out.println("____________________________________________________________");
-        if (taskCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println(" No tasks available!");
         } else {
             System.out.println(" Here are the tasks in your list:");
-            for (int i = 0; i < taskCount; i++) {
-                System.out.println(" " + (i + 1) + ". " + tasks[i]);
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println(" " + (i + 1) + ". " + tasks.get(i));
             }
         }
         System.out.println("____________________________________________________________");
@@ -121,13 +124,13 @@ public class Avocado {
     private static void markTaskAsDone(String input) throws AvocadoException {
         try {
             int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
+            if (taskIndex < 0 || taskIndex >= tasks.size()) {
                 throw new AvocadoException("Oops! Task number out of range.");
             }
-            tasks[taskIndex].markAsDone();
+            tasks.get(taskIndex).markAsDone();
             System.out.println("____________________________________________________________");
             System.out.println(" Nice! I've marked this task as done:");
-            System.out.println("   " + tasks[taskIndex]);
+            System.out.println("   " + tasks.get(taskIndex));
             System.out.println("____________________________________________________________");
         } catch (Exception e) {
             throw new AvocadoException("Oops! Please provide a valid task number to mark as done.");
@@ -137,13 +140,13 @@ public class Avocado {
     private static void unmarkTask(String input) throws AvocadoException {
         try {
             int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
+            if (taskIndex < 0 || taskIndex >= tasks.size()) {
                 throw new AvocadoException("Oops! Task number out of range.");
             }
-            tasks[taskIndex].markAsNotDone();
+            tasks.get(taskIndex).markAsNotDone();
             System.out.println("____________________________________________________________");
             System.out.println(" OK, I've marked this task as not done yet:");
-            System.out.println("   " + tasks[taskIndex]);
+            System.out.println("   " + tasks.get(taskIndex));
             System.out.println("____________________________________________________________");
         } catch (Exception e) {
             throw new AvocadoException("Oops! Please provide a valid task number to unmark.");
