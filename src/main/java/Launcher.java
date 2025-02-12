178e05dd0f340a0cebef00ef2cad061f60ee61a6
lilduckling <naduc3110@gmail.com>
2025-02-12 15:49:42 +0800

    Implement A-Assertions: Add assertions for critical assumptions
    - Added assertions in TaskList.java to validate task index before marking tasks as done.
    
    - Added assertions in Avocado.java to validate task index before marking tasks as done.
    
    - Added assertions in Launcher.java to validate task index before marking tasks as done.
    
    These assertions improve code reliability and maintainability while ensuring key assumptions hold.

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,10 +1,15 @@
 import javafx.application.Application;
+// import jdk.javadoc.internal.tool.Main;
 
 /**
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
     public static void main(String[] args) {
+        assert args != null: "args should not be null";
+
+        assert Main.class != null: "Main class should not be null";
         Application.launch(Main.class, args);
     }
 }
+
